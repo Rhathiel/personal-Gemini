db3bb5ac4adea0675a8e153e3ceb6e9a236e8f71
Rhathiel <hyeok050119@gmail.com>
2025-09-02 20:13:39 +0900

    d

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     setInput("")
     setMessages("")
 
-    const response = await fetch("https://431641535202.netlify.app/.netlify/edge-functions/stream", {
+    const response = await fetch("https://431641535202.netlify.app/api/stream", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
