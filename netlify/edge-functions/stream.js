5e9bc5dfd4856fe444f0d822a0665835a98abb3f
Rhathiel <hyeok050119@gmail.com>
2025-09-02 21:10:01 +0900

    d

--- netlify/edge-functions/stream.js
+++ netlify/edge-functions/stream.js
@@ -42,8 +42,8 @@ export default async function handler(request) {
   (async () => {      
     try {
       const response = await fetch(
-        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:streamGenerateContent?key=" +
-          Deno.env.get("GEMINI_API_KEY"),
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:streamGenerateContent?key=" +
+          Netlify.env.get("GEMINI_API_KEY"),
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
