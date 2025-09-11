5e7135a27d090431af4ce6b4d1e4808c5c8670ae
Rhathiel <hyeok050119@gmail.com>
2025-09-11 21:40:13 +0900

    asd

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build: { outDir: "docs" },
+  base: "/personal-Gemini/",
   plugins: [react()],
 })
