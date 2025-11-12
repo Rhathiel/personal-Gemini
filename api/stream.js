2b9fffad2e2c38619c7dd50eb190db52d8030eeb
Rhathiel <hyeok050119@gmail.com>
2025-11-13 06:22:39 +0900

    asd

--- api/stream.js
+++ api/stream.js
@@ -86,6 +86,7 @@ export default async function handler(req, res) { //fetch 이후 동작
   //전달받은 history로 ai 생성(이전 대화 기억)
 
   console.log("Received request");
+  console.log("Prompt:", prompt);
 
   const stream = new Readable({
     read() {
