e8905b93a47b53bb625b275bdc105a900a352830
Rhathiel <hyeok050119@gmail.com>
2025-11-13 07:55:56 +0900

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 
 function App() {
   useEffect(() => {
-    console.log("version: 1.0.9");
+    console.log("version: 1.0.91");
   }, []);
 
   const [input, setInput] = useState("");
