1a2e00b054bd61e3779567e60d182336393562c0
Rhathiel <hyeok050119@gmail.com>
2025-11-18 05:53:22 +0900

    auto update

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function clearPlaceHolder(){
 
 function App() {
   useEffect(() => {
-    console.log("version: 1.1.08");
+    console.log("version: 1.1.09");
   }, []);
 
   const activeEnter = (e) => {
