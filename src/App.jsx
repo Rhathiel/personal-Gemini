59beefc96f9f86ddcc0cd19504ca552a4f2686e6
Rhathiel <hyeok050119@gmail.com>
2025-11-18 01:33:25 +0900

    auto update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 
 function App() {
   useEffect(() => {
-    console.log("version: 1.0.99");
+    console.log("version: 1.1.00");
   }, []);
 
   const [input, setInput] = useState("");
