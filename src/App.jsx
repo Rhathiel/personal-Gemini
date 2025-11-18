5411d3108a7c55bb6459fc7072e7ec8c97568f7d
Rhathiel <hyeok050119@gmail.com>
2025-11-19 06:59:17 +0900

    auto update

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import styled from 'styled-components';
 const Div = styled.div`
   display: flex;
   flex-direction: row;
+  height: 100%;
 `;
 
 function App() {
--- src/components/monitor/Monitor.jsx
+++ src/components/monitor/Monitor.jsx
@@ -2,11 +2,12 @@ import { Canvas } from '@react-three/fiber';
 import { MMDLoader } from 'three/examples/jsm/loaders/MMDLoader';
 import { Suspense } from 'react';
 import { useLoader } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 import styled from 'styled-components';
 
 const Div = styled.div`
-    top: 50%;
     height: 100%;
+    width: 1000px;
 `;
 
 function MmdModel({ url }) {
@@ -18,9 +19,16 @@ function Monitor() {
 
     return (
         <Div>
-            <Canvas camera={{ position: [0, 10, 30], fov: 40 }}>
+            <Canvas camera={{ position: [0, 30, 30], fov: 45 }}>
                 <ambientLight intensity={1} />
                 <directionalLight position={[10, 10, 10]} intensity={1} />
+                    <OrbitControls
+                    enableDamping
+                    dampingFactor={0.1}
+                    enablePan={true}
+                    enableZoom={true}
+                    target={[0, 10, 0]} // 모델 중심 쪽으로 맞추고 싶으면 여기 값 조정
+                    />
                 <Suspense fallback={null}>
                     <MmdModel url="/personal-Gemini/model/PMX/kanata.pmx"/>
                 </Suspense>
