5b3abf86612f65940c0465fef1902e595b9522b1
Rhathiel <hyeok050119@gmail.com>
2025-11-22 22:31:18 +0900

    asd

--- src/components/chat/LoadingScreeen.jsx
+++ src/components/chat/LoadingScreeen.jsx
@@ -4,9 +4,10 @@ const Div = styled.div`
   position: fixed;
   top: 5%;
   bottom: 5%;
-  left: 240px;  
-  right: 470px;    
-  background: #705454ff;
+  left: 300px;  
+  right: 500px;    
+  border-radius: 5%;
+  background: #272727ff;
   z-index: 10;
 `;
 
