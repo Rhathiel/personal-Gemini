4ae07f1943e76b3a839393c9c1ba4a9b3cb24bb7
rhathiel <hyeok050119@gmail.com>
2025-12-13 02:19:22 +0900

    auto update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-Gemini/'
+  base: '/personal-Gemini/',
+  build: {
+    outDir: "docs",
+  },
 })
