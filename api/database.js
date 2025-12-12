41fb116a37f779b2647cea1544a6953fb27b8a2f
rhathiel <hyeok050119@gmail.com>
2025-12-13 02:29:46 +0900

    auto update

--- api/database.js
+++ api/database.js
@@ -1,20 +1,19 @@
 import { Redis } from '@upstash/redis';
 import * as utils from './utils.ts';
-import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export const redis = new Redis({
   url: process.env.KV_REST_API_URL,
   token: process.env.KV_REST_API_TOKEN,
 });
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
 
-  const corsHeaders: Record<string, string> = {
+  const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "POST, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
   };
-  const headers: Record<string, string> = {
+  const headers = {
       ...corsHeaders,
       "Content-Type": "application/json",
       "Cache-Control": "no-cache",
@@ -34,16 +33,8 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     return;
   } 
 
-  interface RequestBody {
-    request: number;
-    sessionId?: string;
-    data?: session | Array<message>;
-    oldData?: session;
-    newData?: session;
-  }
-
   //input 받음
-  const body: RequestBody = req.body;
+  const body = req.body;
 
   for (const key in headers){
     res.setHeader(key, headers[key]);
@@ -65,7 +56,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     case 3: {
       //load message
       const raw = await redis.lrange(`messages:${body.sessionId}`, 0, -1);
-      const list: Array<message> = raw.map(str => utils.parseText(str));
+      const list = raw.map(str => utils.parseText(str));
       res.status(200).json(list);
       return;
     }
@@ -83,7 +74,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     }
     case 6: {
       //edit sessionList
-      const idx: number = await redis.lpos("sessionList", utils.stringifyJson(body.oldData));
+      const idx = await redis.lpos("sessionList", utils.stringifyJson(body.oldData));
       await redis.lset("sessionList", idx, utils.stringifyJson(body.newData));
       res.status(200).json({ ok: true })
       return;
@@ -91,7 +82,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     case 7: {
       //load sessionList
       const raw = await redis.lrange("sessionList", 0, -1);
-      const list: Array<session> = raw.map(str => utils.parseText(str));
+      const list = raw.map(str => utils.parseText(str));
       res.status(200).json(list);
       return;
     }
