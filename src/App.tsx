9dbe667319849a52dc1e93740792f58b580d0fc8
rhathiel <hyeok050119@gmail.com>
2025-12-14 03:45:42 +0900

    asdasd

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useEffect } from 'react';
 import SideBar from './components/sidebar/SideBar.tsx';
 import Monitor from './components/monitor/Monitor.tsx';
 import MainView from './components/MainView.tsx';
 
 function App() {
-
-  //버전
-  useEffect(() => {
-    console.log("version: 1.2.3");
-  }, []);
 
   return (
     <>
