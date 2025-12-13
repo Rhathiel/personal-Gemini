92d350abfd4c2a62b4f558c5710578e939009800
rhathiel <hyeok050119@gmail.com>
2025-12-14 06:05:29 +0900

    ad

--- src/components/chat/LoadingScreen.tsx
+++ src/components/chat/LoadingScreen.tsx
@@ -6,3 +6,7 @@ function LoadingScreen (){
 }
 
 export default LoadingScreen;
+
+//일단은 renderer default에 걸려있긴 한데.. 좀더 홣용방안을 생각해야할듯.
+
+//일단 지금 방식처럼 unmount - mount는 개애바고, MainView에 loading state를 두고 js문으로 오버레이 하면 될듯.
