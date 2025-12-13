2a5dbb5a487729053643e08d64060d4560cb92d4
rhathiel <hyeok050119@gmail.com>
2025-12-13 18:10:34 +0900

    gdgd

--- api/stream.js
+++ api/stream.js
@@ -90,7 +90,7 @@ export default async function handler(req, res) {
 
   const output = await createOutput(chat, userMsg.parts);
   if(typeof output?.[Symbol.asyncIterator] !== "function"){ 
-    res.status(429).json(output);
+    res.status(200).json(output);
     return;
   }
 
