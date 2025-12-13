d378856cf10633a21664b8d760f81c9a55ccb205
rhathiel <hyeok050119@gmail.com>
2025-12-14 03:17:15 +0900

    asdsad

--- src/stores/uiStateStore.ts
+++ src/stores/uiStateStore.ts
@@ -2,7 +2,11 @@ import { create } from 'zustand';
 import * as utils from '../lib/utils.ts'
 
 export const useUiStateStore = create<UiStateStore>((set) => ({ 
-    uiState: utils.parseText(sessionStorage.getItem("uiState")),
+    uiState: utils.parseText(sessionStorage.getItem("uiState")) || {
+        mode: "home",
+        sessionId: null,
+        sideIsOpened: false
+    },
     setUiState: (uiState) => set((prev) => 
         ({uiState: { ...prev.uiState, ...uiState } } )),
     toggleSideIsOpened: () => set((prev) => ({ 
