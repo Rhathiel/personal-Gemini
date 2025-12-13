2e286086a6346d00821f2dd57f52a4a6ce069313
rhathiel <hyeok050119@gmail.com>
2025-12-14 03:23:47 +0900

    asdasd

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import { useEffect } from 'react';
 import SideBar from './components/sidebar/SideBar.tsx';
 import Monitor from './components/monitor/Monitor.tsx';
-import * as utils from './lib/utils.ts'
 import MainView from './components/MainView.tsx';
-import { useUiStateStore } from './stores/uiStateStore.ts';
 
 function App() {
-  const { uiState } = useUiStateStore();
 
   //버전
   useEffect(() => {
     console.log("version: 1.2.3");
   }, []);
-
-  //uiState 저장 로직
-  useEffect(() => {
-    sessionStorage.setItem("uiState", utils.stringifyJson(uiState));
-  }, [uiState]); 
 
   return (
     <>
