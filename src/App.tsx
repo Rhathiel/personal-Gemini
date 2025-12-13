af4193034eef49e43c467d42b87f479f7b495243
rhathiel <hyeok050119@gmail.com>
2025-12-14 03:08:35 +0900

    asdsad

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import MainView from './components/MainView.tsx';
 import { useUiStateStore } from './stores/uiStateStore.ts';
 
 function App() {
-  const { uiState } = useUiStateStore();
+  const { uiState, setUiState } = useUiStateStore();
+
+  setUiState(utils.parseText(sessionStorage.getItem("uiState")));
 
   //버전
   useEffect(() => {
